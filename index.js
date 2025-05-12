a3d945f90f8440a02c61260234f5155455af56fb
Moustapha <amroune.moustapha@gmail.com>
2025-05-12 16:04:05 +0200

    add json file data, html content, dimensions chart

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(d3.version)
+const width = 800;
+const height = 400;
+const padding = 40;
+
+const svg = d3.select("#chart").attr("width", width).attr("height", height);
+
+const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+d3.json(url).then(data => {
+    const dataset = data.data;
+    console.log(dataset)
+} )
+
