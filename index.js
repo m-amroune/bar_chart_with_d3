d7532377b86bb0930f1c1832d28cf93dd540f7be
Moustapha <amroune.moustapha@gmail.com>
2025-05-12 17:05:50 +0200

    add chart bars

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ d3.json(url).then(data => {
  
 
 
-const xScale = d3.scaleTime().domain([dataset[0][0], new Date(dataset[dataset.length - 1][0])]).range([padding], [width - padding]);
+const xScale = d3.scaleTime().domain([new Date(dataset[0][0]), new Date(dataset[dataset.length - 1][0])]).range([padding, width - padding]);
+
+
 
   const yScale = d3.scaleLinear().domain([0, d3.max(dataset, d => d[1])]).range([height - padding, padding]);
 
@@ -20,4 +22,10 @@ const yAxis = d3.axisLeft(yScale);
  svg.append("g").attr("id", "x-axis").attr("transform", `translate(0, ${height - padding})`).call(xAxis);
 
   svg.append("g").attr("id", "y-axis").attr("transform", `translate(${padding}, 0)`).call(yAxis);
+
+  const barWidth = (width - 2 * padding) / dataset.length - 1;
+
+   svg.selectAll("rect").data(dataset).enter().append("rect").attr("class", "bar").attr("x", d => xScale(new Date(d[0]))).attr("y", d => yScale(d[1])).attr("width", barWidth).attr("height", d => height - padding - yScale(d[1])).attr("data-date", d => d[0]).attr("data-gdp", d => d[1]).attr("fill", "steelblue")
+
+
 } )
